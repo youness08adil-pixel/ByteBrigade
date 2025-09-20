44f618a24591eae91953409d3093da3b3a03dfd3
youness08adil-pixel <youness08adil@gmail.com>
2025-09-20 15:36:47 +0100

    Update page.tsx

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,3 +1,5 @@
+export const dynamic = 'force-dynamic';
+export const fetchCache = 'force-no-store';
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import prisma from "../lib/db";
